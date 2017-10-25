ecbf45e675b2eb431b9a5d5642a74fc487b50012
arianahmadian <32783319+arianahmadian@users.noreply.github.com>
2017-10-25 15:05:09 -0700

    lesson 1 upl

---
+++ ExerciseOne.java
@@ -0,0 +1,39 @@
+package exercises;
+
+/*
+Lesson _01
+In this exercise, we will use variables to print a sample ID card
+A sample ID Card will look like this:
+***************************************
+Surya Madhan         Millennium Falcons
+Programming                   Team 3647
+Proficient in Java            2017-2018
+***************************************
+Good Luck Completing the exercise!
+*/
+
+/*
+ * @author Surya
+ */
+
+public class ExerciseOne 
+{
+
+    public static void main(String[] args) 
+	{
+		ExerciseOne form = new ExerciseOne();
+		
+		
+		
+		String name, teamName, department, somethingToBragAbout, schoolYear;
+		int teamNumber;
+		
+		System.out.println("***************************************");
+		System.out.println("Arian                Millenium Falcons");
+		System.out.println("Programming          Team 3647");
+		System.out.println("Proficient in edit   2017-2018");
+		System.out.println("***************************************");
+  	}
+ 
+    
+}
